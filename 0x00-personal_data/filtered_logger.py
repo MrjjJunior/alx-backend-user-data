f1bb7dfaf7d0f61da25b36760cdddcd6ae644abe
Tshepiso <tlhongtshepiso2@gmail.com>
2024-11-06 23:16:50 +0200

    : ]

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" filter """
+from typing import List, Union
+import re
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        original_message = super(RedactingFormatter, self).format(record)
+        return filter_datum(self.fields, self.REDACTION, original_message, self.SEPARATOR)
+
+
+def filter_datum(fields: List[str],
+                redaction: str,
+                message: str,
+                separator: str
+                ) -> str:
+    '''Returns log message'''
+    pattern = f'({"|".join(fields)})=[^;]*'
+    replacement = rf"\1={redaction}"
+    return re.sub(pattern, replacement, message)
