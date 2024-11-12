cf9bd73537dc470b16c29f2cb0d548f23d4244c0
Tshepiso <tlhongtshepiso2@gmail.com>
2024-11-12 23:34:00 +0200

    Base64 task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,18 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     ''' basic auth
     '''
-    pass
+    
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        ''' extract base64 authorization header
+        '''
+        if authorization_header is None:
+            return
+        if not isinstance(authorization_header, str):
+            return
+        if not authorization_header.startswith('Basic '):
+            return
+        
+        value = authorization_header.split(' ')[1]
+        return value
+        
+        
