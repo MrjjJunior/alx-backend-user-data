c86141f54168ba0e3dca4871a5b26abd33b2c473
Tshepiso <tlhongtshepiso2@gmail.com>
2024-11-12 18:48:43 +0200

    Auth class pycodestyle

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,7 +7,6 @@ from typing import List, TypeVar
 class Auth():
     '''Authentication'''
 
-
     def require_auth(
             self,
             path: str,
@@ -16,11 +15,9 @@ class Auth():
         '''Check if the request contains a valid JWT'''
         return False
 
-
     def authorization_header(self, request=None) -> str:
         '''Get the Authorization header value from the request'''
         return None
-    
 
     def current_user(self, request=None) -> TypeVar('User'):
         '''Get the current user from the request'''
