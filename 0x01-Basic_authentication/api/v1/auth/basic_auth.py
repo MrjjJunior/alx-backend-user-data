139d244d094e222f0d51921b2f9abbd98f3d583e
Tshepiso <tlhongtshepiso2@gmail.com>
2024-11-13 00:17:40 +0200

    decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 ''' Basic Auth
 '''
+from base64 import b64decode
 from api.v1.auth.auth import Auth
 
 
@@ -23,3 +24,20 @@ class BasicAuth(Auth):
 
         value = authorization_header.split(' ')[1]
         return value
+    
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str
+            ) -> str:
+        ''' decode base64
+        '''
+        if not base64_authorization_header:
+            return
+        if not isinstance(base64_authorization_header, str):
+            return
+        try:
+            encoded_base64 = b64decode(base64_authorization_header)
+            decoded_base64 = encoded_base64.decode('utf-8')
+        except Exception:
+            return
+        return decoded_base64
