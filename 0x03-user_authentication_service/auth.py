acf9482d46cc707d2a506a1fb622d71d3fa5619f
Tshepiso <tlhongtshepiso2@gmail.com>
2024-11-20 19:35:18 +0200

    hashing password & pycodestyle

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-'''  ''' 
+''' Modules hashes passwords '''
 import bcrypt
+from bcrypt import hashpw, gensalt
 
 
 def _hash_password(password: str) -> bytes:
     ''' Gets password than hases it using bcrypt '''
     pwd = password.encode('utf-8')
-    return bcrypt.hashpw(pwd, bcrypt.gensalt())
+    return hashpw(pwd, gensalt())
